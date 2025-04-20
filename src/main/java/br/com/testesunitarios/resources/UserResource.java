96b754b758857a8fc05efb38b73bb623d7585aa5
oxyCLSA <carlossouand@gmail.com>
2025-04-20 20:05:38 -0300

    retornando o dto para o user resource

--- src/main/java/br/com/testesunitarios/resources/UserResource.java
+++ src/main/java/br/com/testesunitarios/resources/UserResource.java
@@ -2,7 +2,9 @@ package br.com.testesunitarios.resources;
 
 
 import br.com.testesunitarios.domain.User;
+import br.com.testesunitarios.domain.dto.UserDTO;
 import br.com.testesunitarios.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id) {
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
     }
 }
