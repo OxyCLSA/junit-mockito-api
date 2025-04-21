80a70c09cda8487adf9dae9e5646c0b6662073a7
oxyCLSA <carlossouand@gmail.com>
2025-04-20 22:26:59 -0300

    excecao para email ja cadastrado na atualizacao

--- src/main/java/br/com/testesunitarios/resources/UserResource.java
+++ src/main/java/br/com/testesunitarios/resources/UserResource.java
@@ -42,4 +42,11 @@ public class UserResource {
                 .fromCurrentRequest().path("/{id}").buildAndExpand(service.create(obj).getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> update(@PathVariable Long id, @RequestBody UserDTO obj) {
+        obj.setId(id);
+        User newObj = service.update(obj);
+        return ResponseEntity.ok().body(mapper.map(newObj, UserDTO.class));
+    }
 }
