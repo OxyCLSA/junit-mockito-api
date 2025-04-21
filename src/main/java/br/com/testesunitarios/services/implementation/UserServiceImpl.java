506363e7c83a4f57afeffb59544fb18f42a1e0fe
oxyCLSA <carlossouand@gmail.com>
2025-04-20 22:00:53 -0300

    excecao para email ja cadastrado

--- src/main/java/br/com/testesunitarios/services/implementation/UserServiceImpl.java
+++ src/main/java/br/com/testesunitarios/services/implementation/UserServiceImpl.java
@@ -4,6 +4,7 @@ import br.com.testesunitarios.domain.User;
 import br.com.testesunitarios.domain.dto.UserDTO;
 import br.com.testesunitarios.repositories.UserRepository;
 import br.com.testesunitarios.services.UserService;
+import br.com.testesunitarios.services.exceptions.DataIntegratyViolationException;
 import br.com.testesunitarios.services.exceptions.ObjectNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +34,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User create(UserDTO obj) {
+        findByEmail(obj);
         return repository.save(mapper.map(obj, User.class));
+    }
+
+    private void findByEmail(UserDTO obj) {
+      Optional<User> user = repository.findByEmail(obj.getEmail());
+      if(user.isPresent()) {
+          throw  new DataIntegratyViolationException("Email já cadastrado no sistema");
+      }
     }
 }
